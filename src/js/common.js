9ab3c0887db28235689fca909b65d4f998843907
alpscloud <winde100kg@gmail.com>
2022-07-26 21:58:35 +0300

    prebuild

--- src/js/common.js
+++ src/js/common.js
@@ -17,5 +17,62 @@ $(document).ready(function() {
 		}, 200);
 	});
 
+	$('.js-open-mobile-menu-btn').on('click', function(e) {
+		e.preventDefault();
+
+		$('html').toggleClass('is-fixed');
+		$(this).toggleClass('is-active');
+		$('.js-nav').toggleClass('is-opened');
+	});
+
+
+	$('.form-input').on('focus', function() {
+		var value = $(this).val();
+
+		if($(this).hasClass('is-error')) {
+			$(this).removeClass('is-error');
+		}
+
+		if (!$(this).val() || $(this).val() == "") {
+			$(this).next('.form-input-placeholder').css('display', 'none');
+		}
+
+	});
+
+	$('.form-input').on('blur', function() {
+		var value = $(this).val();
+
+		if ($(this).val() || $(this).val() !== "") {
+			$(this).next('.form-input-placeholder').css('display', 'none');
+		} else {
+			$(this).next('.form-input-placeholder').css('display', 'block');
+		}
+
+	});
+
+	$('.js-select').on('click', function(e) {
+		$(this).toggleClass('is-active');
+	});
+
+	$('.js-select .form-select-option').on('click', function(e) {
+		var currencyLogo = $(this).attr('data-currency-logo');
+		var currencyAbbr = $(this).attr('data-currency-abbr');
+		var currencyTitle = $(this).attr('data-currency-name');
+		var currencyKey = $(this).attr('data-currency-key');
+
+		var select = $(this).parents('.js-select');
+
+		select.find('.js-select-input').val(currencyKey).trigger('change');
+		select.find('.js-select-choiced .exchange-heading__currency-logo img').attr('src', currencyLogo);
+		select.find('.js-select-choiced .main-currency__abbr').html(currencyAbbr);
+		select.find('.js-select-choiced .main-currency__title').html(currencyTitle);
+	});
+
+	$(document).mouseup(function (e) {
+		var select = $('.js-select');
+		if (select.has(e.target).length === 0){
+			select.removeClass('is-active');
+		}
+	});
 
 });
